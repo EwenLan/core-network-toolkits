06e3733455b0bf67195010df4842cb2d79a3b7ec
EwenLan <ewen_lan@outlook.com>
2022-06-21 17:11:24 +0800

    added upload and download

--- src/pages/home/mmlmirror.tsx
+++ src/pages/home/mmlmirror.tsx
@@ -1,5 +1,6 @@
-import { Button } from "antd"
+import { Button, Row, Upload } from "antd"
 import TextArea from "antd/lib/input/TextArea"
+import { RcFile } from "antd/lib/upload"
 import React, { useState } from "react"
 import { ScriptTypeFromText } from "../../common/mml/mediation"
 import { Script } from "../../common/mml/script"
@@ -15,6 +16,7 @@ export function MMLMirror() {
 function MMLMirrorApp() {
     const [configurationSection, setConfigurationSection] = useState("")
     const [formattedScript, setFormattedScript] = useState("")
+    const [downloadDisable, setDownloadDisable] = useState(true)
     const handleInputConfigurationSection = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setConfigurationSection(e.target.value)
     }
@@ -22,16 +24,47 @@ function MMLMirrorApp() {
         const inputScript = new Script(ScriptTypeFromText(configurationSection))
         inputScript.Sort()
         setFormattedScript(inputScript.ToString())
+        setDownloadDisable(false)
+    }
+    const handleUploadFile: ((file: RcFile) => boolean) = (file) => {
+        const reader = new FileReader()
+        reader.readAsText(file)
+        reader.onload = (res) => {
+            if (res.target) {
+                setConfigurationSection(res.target.result as string)
+            }
+        }
+        return false
+    }
+    const downloadFormattedFile = () => {
+        const downloadLink = document.createElement("a")
+        downloadLink.download = "Formatted Script.txt"
+        const blob = new Blob([formattedScript])
+        downloadLink.href = URL.createObjectURL(blob)
+        downloadLink.click()
     }
     return (
         <>
             <KeyValuePair title="Configuration Section" value={
+                <Upload action="" accept="plain/text" beforeUpload={handleUploadFile} showUploadList={false}>
+                    <Button children="Upload" />
+                </Upload>
+            }
+                direction={KeyValuePairDirection.horizontal}
+            />
+            <KeyValuePair title="" value={
                 <TextArea allowClear={true}
                     onChange={handleInputConfigurationSection} rows={6}
+                    value={configurationSection}
                 />}
                 direction={KeyValuePairDirection.vertical}
             />
-            <KeyValuePair title="Operation" value={<Button children="Convert" type="primary" onClick={handleClick} />} />
+            <KeyValuePair title="Operation" value={
+                <Row>
+                    <Button children="Convert" type="primary" onClick={handleClick} />
+                    <Button children="Download" onClick={downloadFormattedFile} disabled={downloadDisable} />
+                </Row>
+            } />
             <KeyValuePair title="Formatted Script" value={
                 <TextArea allowClear={true} value={formattedScript} rows={6} />}
                 direction={KeyValuePairDirection.vertical}
